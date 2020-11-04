6ab85c0bf845d92535a74e3184494dccf96d214e
Estelle <estelle.thou@gmail.com>
2020-11-04 10:57:38 +0100

    send and email to confirm registration

---
+++ free_ads/HowToCook/resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+# Welcome to HowToCook
+
+Thanks for joining us ! <br>
+This is a community of food lovers and people who wants to share their skills.<br>
+Be ready, grap your cooking tools and let's go !<br>
+
+@component('mail::button', ['url' => ''])
+Button Text
+@endcomponent
+
+All the best, <br>
+Estelle
+@endcomponent
